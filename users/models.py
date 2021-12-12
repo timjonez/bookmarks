af057c06655a8e74cc129f8870f1675e81eb9e57
Tim Jones <timjonez@protonmail.com>
2021-12-11 22:02:18 -0600

    [users/models] add methods to the user class

--- users/models.py
+++ users/models.py
@@ -1,7 +1,10 @@
+import pyjwt
+import bcrypt
 import ormar
 import datetime
 import sqlalchemy
 from bookmarker.db import BaseMeta
+from bookmarker.settings import settings
 
 
 class Users(ormar.Model):
@@ -14,3 +17,13 @@ class Users(ormar.Model):
     is_active: bool = ormar.Boolean(default=True)
     is_admin: bool = ormar.Boolean(default=False)
     joined_date: datetime.datetime = ormar.DateTime(server_default=sqlalchemy.func.now())
+
+    def password_is_valid(self, password):
+        if bcrypt.checkpw(password, self.password):
+            return True
+        return False
+
+    def login(self, password):
+        if self.password_is_valid(password):
+            return pyjwt.encode(self.email, settings.secret_key)
+        return None
