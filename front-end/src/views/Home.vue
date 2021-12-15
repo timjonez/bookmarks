8f285222105c3fd22561e01ebf346e6920049f76
Tim Jones <timjonez@protonmail.com>
2021-12-14 18:55:50 -0600

    [front-end] add vue front end starter

---
+++ front-end/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Hello Bookmarker</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
