51c3237f60ba594b516e8847e52d9964b362db03
Tim Jones <timjonez@protonmail.com>
2021-12-15 21:27:04 -0600

    [front-end] remove lesson pages, and add bookmarks

--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <h1>Home Page</h1>
+  <div>
+    <h1>Home Page</h1>
+    <bookmark-item v-for="bookmark in bookmarks" :key="bookmark.id" :bookmark="bookmark" />
+  </div>
 </template>
 
 <script>
+import BookmarkItem from '../components/BookmarkItem.vue'
+
 export default {
-  name: 'IndexPage'
+  name: 'BookmarkList',
+  components: {
+    BookmarkItem
+  },
+  data () {
+    return {
+      bookmarks: [...this.$store.state.bookmarks]
+    }
+  }
 }
 </script>
--- front-end/store/index.js
+++ front-end/store/index.js
@@ -1,9 +1,35 @@
 export const state = () => ({
-  lessons: [
+  bookmarks: [
     {
       id: 1,
-      name: 'Lesson 1',
-      body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto laborum, iure odio voluptas'
+      name: 'Google',
+      url: 'https://google.com',
+      icon: 'https://www.google.com/favicon.ico'
+    },
+    {
+      id: 2,
+      name: 'Facebook',
+      url: 'https://facebook.com',
+      icon: 'https://facebook.com/favicon.ico'
+    },
+    {
+      id: 3,
+      name: 'Amazon',
+      url: 'https://amazon.com',
+      icon: 'https://amazon.com/favicon.ico'
     }
   ]
 })
+
+export const actions = {
+  editBookmark (x, payload) {
+    console.log('edit bookmark clicked')
+    console.log(x)
+    console.log(payload)
+  },
+  deleteBookmark (x, payload) {
+    console.log('delete bookmark clicked')
+    console.log(x)
+    console.log(payload)
+  }
+}
