f2d51b3b4f24d2a77dd6aae920b3ead99d9086f8
Tim Jones <timjonez@protonmail.com>
2021-12-28 15:36:10 -0600

    [migrations/env] update project name

--- migrations/env.py
+++ migrations/env.py
@@ -4,7 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from project.db import BaseMeta
+from bookmarker.db import BaseMeta
 from users.models import Users
 from bookmarks.models import Bookmarks
 
