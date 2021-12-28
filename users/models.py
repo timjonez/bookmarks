6554738605e9461f4404b0fd787d37638e7ae025
Tim Jones <timjonez@protonmail.com>
2021-12-27 21:09:10 -0600

    [users/models] add user methods for authentication

--- users/models.py
+++ users/models.py
@@ -1,10 +1,10 @@
-import pyjwt
+import jwt
 import bcrypt
 import ormar
-import datetime
 import sqlalchemy
 from bookmarker.db import BaseMeta
 from bookmarker.settings import settings
+from datetime import datetime, timedelta, timezone
 
 
 class Users(ormar.Model):
@@ -16,14 +16,20 @@ class Users(ormar.Model):
     password: str = ormar.String(max_length=128)
     is_active: bool = ormar.Boolean(default=True)
     is_admin: bool = ormar.Boolean(default=False)
-    joined_date: datetime.datetime = ormar.DateTime(server_default=sqlalchemy.func.now())
+    joined_date: datetime = ormar.DateTime(server_default=sqlalchemy.func.now())
 
-    def password_is_valid(self, password):
-        if bcrypt.checkpw(password, self.password):
-            return True
-        return False
+    async def create(self, *args, **kwargs):
+        self.password = await self.hash_password(self.password)
+        return await self.save(*args, **kwargs)
 
-    def login(self, password):
-        if self.password_is_valid(password):
-            return pyjwt.encode(self.email, settings.secret_key)
+    async def password_is_valid(self, password):
+        return bcrypt.checkpw(password.encode(), self.password.encode())
+
+    async def login(self, password):
+        if await self.password_is_valid(password):
+            expire_date = datetime.now(tz=timezone.utc) + timedelta(hours=3)
+            return jwt.encode({"email": self.email, "exp": expire_date}, settings.secret_key, algorithm="HS256")
         return None
+
+    async def hash_password(self, raw_password):
+        return bcrypt.hashpw(raw_password.encode(), bcrypt.gensalt(rounds=settings.salt_rounds))
