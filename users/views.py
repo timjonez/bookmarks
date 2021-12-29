8e0b855e774cc959e559a984a0474b6fdee69bf9
Tim Jones <timjonez@protonmail.com>
2021-12-29 15:04:13 -0600

    [users/views] add endpoints for users and login

--- users/views.py
+++ users/views.py
@@ -1,15 +1,36 @@
-from fastapi import HTTPException, APIRouter
-from .models import Users
+from fastapi import HTTPException, APIRouter, Request, Depends
+from .models import User, UserWriteModel, UserReadModel
+from typing import List
+from .helpers import is_authorized
+from pydantic import BaseModel
 
 router = APIRouter()
 
-RequestUser = Users.get_pydantic(exclude={"id", "is_active", "is_admin", "joined_date"})
-ResponseUser = Users.get_pydantic(exclude={"password"})
-
-@router.post("/account/create/", response_model=ResponseUser)
-async def create_user(user: RequestUser):
+@router.post("/account/create/", response_model=UserReadModel)
+async def create_user(user: UserWriteModel):
     user_dict = user.dict()
     email = user_dict.get("email")
-    if not await Users.objects.get_or_none(email=email):
-        return await Users(**user_dict).save()
+    if not await User.objects.get_or_none(email=email):
+        return await User(**user_dict).save()
     raise HTTPException(status_code=400, detail="Email already registered")
+
+@router.get("/users", response_model=List[UserReadModel])
+async def get_users():
+    return await User.objects.all()
+
+@router.get("/user/{id}", response_model=UserReadModel)
+async def get_users(id: int, request: Request, user: dict = Depends(is_authorized)):
+    if not await User.objects.get_or_none(id=id):
+        raise HTTPException(status_code=404, detail="User not found")
+    return await User.objects.get(id=id)
+
+@router.post("/account/login/")
+async def create_user(user: UserWriteModel):
+    user_dict = user.dict()
+    email = user_dict.get("email")
+    user = await User.objects.get_or_none(email=email)
+    if user:
+        token = await user.login(user_dict.get("password"))
+        if token is not None:
+            return {"token": token}
+    raise HTTPException(status_code=400, detail="Credentials provided are invalid")
