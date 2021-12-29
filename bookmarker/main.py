813d2b8fb0065eb2df976828f9571398e8cb09a9
Tim Jones <timjonez@protonmail.com>
2021-12-29 15:00:53 -0600

    [bookmarker/main] remove some extra stuff

--- bookmarker/main.py
+++ bookmarker/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 import uvicorn
-import bookmarker
 
 from bookmarker.db import database
 from users.views import router as user_router
@@ -21,12 +20,6 @@ async def shutdown():
 
 app.include_router(user_router)
 app.include_router(bookmark_router)
-
-
-@app.get('/')
-async def home():
-    return {"message": "this is a test"}
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
