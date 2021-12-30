87e2e4d40af0a4a2fc482c9ddbe020d92d63237b
Tim Jones <timjonez@protonmail.com>
2021-12-30 13:48:45 -0600

    [user/helpers] add auth func to that returns user object

--- users/helpers.py
+++ users/helpers.py
@@ -1,5 +1,6 @@
 from fastapi import HTTPException, Request
 from bookmarker.settings import settings
+from .models import User
 import jwt
 
 def is_authorized(request: Request):
@@ -8,8 +9,16 @@ def is_authorized(request: Request):
     if auth_token is not None:
         try:
             data = jwt.decode(auth_token, settings.secret_key, algorithms="HS256")
-            return data.get("email")
+            return True
         except Exception as exc:
             if type(exc) == jwt.ExpiredSignatureError:
                 msg = "Not authorized - token is expired"
     raise HTTPException(status_code=401, detail=msg)
+
+
+async def authorization(request: Request) -> User:
+    if is_authorized(request):
+        auth_token = request.headers.get("authorization")
+        data = jwt.decode(auth_token, settings.secret_key, algorithms="HS256")
+        return await User.objects.get(email=data.get("email"))
+    raise HTTPException(status_code=401, detail="Not authorized")
