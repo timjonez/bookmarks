3534a7211c4fc4e970fef74f2baa16e2babb27b6
Tim Jones <timjonez@protonmail.com>
2021-12-30 22:33:00 -0600

    [front-end/store/index] adjust store to real backend

--- front-end/store/index.js
+++ front-end/store/index.js
@@ -1,35 +1,58 @@
+import axios from 'axios'
+import { BASEURL } from '../constants'
+
 export const state = () => ({
-  bookmarks: [
+  auth: {
+    userLoggedIn: false,
+    showAuthModal: false,
+    token: null
+  },
+  errors: [
     {
-      id: 1,
-      name: 'Google',
-      url: 'https://google.com',
-      icon: 'https://www.google.com/favicon.ico'
-    },
-    {
-      id: 2,
-      name: 'Facebook',
-      url: 'https://facebook.com',
-      icon: 'https://facebook.com/favicon.ico'
-    },
-    {
-      id: 3,
-      name: 'Amazon',
-      url: 'https://amazon.com',
-      icon: 'https://amazon.com/favicon.ico'
+      title: 'Invalid credentials',
+      details: 'The email/password that you provided are invalid. Please try again',
+      seen: false
     }
-  ]
+  ],
+  bookmarks: []
 })
 
 export const actions = {
-  editBookmark (x, payload) {
-    console.log('edit bookmark clicked')
-    console.log(x)
-    console.log(payload)
+  async login (context, payload) {
+    try {
+      const userCred = await axios.post(
+        BASEURL + '/account/login/',
+        {
+          email: payload.target.email.value,
+          password: payload.target.password.value
+        }
+      )
+      context.commit('setToken', userCred.data.token)
+    } catch (error) {
+      console.log(error.response.data.detail)
+    }
   },
-  deleteBookmark (x, payload) {
-    console.log('delete bookmark clicked')
-    console.log(x)
+  editBookmark (context, payload) {
+    context.commit('editBookmark', payload)
+  },
+  saveBookmark (context, payload) {
+    context.commit('saveBookmark', payload.target.name)
+  },
+  deleteBookmark (context, payload) {
     console.log(payload)
+  }
+}
+
+export const mutations = {
+  editBookmark (state, bookmark) {
+    const bookmarkIndex = state.bookmarks.findIndex(item => item.id === bookmark.id)
+    state.bookmarks[bookmarkIndex].editing = !state.bookmarks[bookmarkIndex].editing
+  },
+  saveBookmark (state, bookmark) {
+    console.log(bookmark)
+  },
+  setToken (state, token) {
+    state.auth.token = token
+    state.auth.userLoggedIn = true
   }
 }
