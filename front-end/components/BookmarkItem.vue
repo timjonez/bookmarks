81cca05f80fe55fecbda288ba910f217af68c229
Tim Jones <timjonez@protonmail.com>
2021-12-30 22:34:54 -0600

    [front-end/compontents/BookmarkItem] add logic to toggle each form

--- front-end/components/BookmarkItem.vue
+++ front-end/components/BookmarkItem.vue
@@ -1,24 +1,41 @@
 <template>
-  <a :href="bookmark.url" class="bookmark-list-item" target="_blank">
-    <img class="favicon" :src="bookmark.icon" alt="">
-    <p>{{ bookmark.name }}</p>
-    <div class="bookmark-actions">
-      <button class="bg-green-700 text-gray-100" @click.prevent="editBookmark">Edit</button>
-      <button class="bg-red-600 text-gray-100" @click.prevent="deleteBookmark">X</button>
-    </div>
-  </a>
+  <div>
+    <a v-if="!bookmark.editing" :href="bookmark.url" class="bookmark-list-item" target="_blank">
+      <img class="favicon" :src="bookmark.icon" alt="">
+      <p>{{ bookmark.title }}</p>
+      <div class="bookmark-actions">
+        <button class="bg-green-700 text-gray-100" @click.prevent="editBookmark(bookmark)">Edit</button>
+        <button class="bg-red-600 text-gray-100" @click.prevent="deleteBookmark(bookmark)">X</button>
+      </div>
+    </a>
+    <ValidationProvider v-else method="post" class="bookmark-list-item">
+      <img class="favicon" :src="bookmark.icon" alt="">
+      <input type="text" :value="bookmark.title" name="title" @change="saveBookmark">
+      <div class="bookmark-actions">
+        <button class="bg-green-700 text-gray-100" type="submit">Save</button>
+        <button class="bg-red-600 text-gray-100" @click.prevent="deleteBookmark(bookmark)">X</button>
+      </div>
+    </ValidationProvider>
+  </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import { ValidationProvider } from 'vee-validate'
 
 export default {
   name: 'BookmarkItem',
+  components: {
+    ValidationProvider
+  },
   props: {
     bookmark: Object
   },
   methods: {
-    ...mapActions(['editBookmark', 'deleteBookmark'])
+    ...mapActions(['editBookmark', 'deleteBookmark', 'saveBookmark']),
+    updateBookmark (e) {
+      this.$store.commit('saveBookmark', e.target.value)
+    }
   }
 }
 </script>
@@ -28,7 +45,7 @@ export default {
   display: flex;
   flex-direction: row;
   padding:  .4rem;
-  background-color: rgb(167, 167, 167);
+  background-color: rgb(232, 232, 232);
   margin: .5rem;
 }
 
