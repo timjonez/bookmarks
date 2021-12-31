42ce751397900f5df0bcb05e70bf1312ff94efdb
Tim Jones <timjonez@protonmail.com>
2021-12-30 22:36:05 -0600

    [front-end/pages/login] add login page

---
+++ front-end/pages/login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <NuxtLink to="/">Home</NuxtLink>
+    <h1>Login</h1>
+    <form method="post" @submit.prevent="login">
+      <label for="email">Email</label>
+      <input type="email" name="email" placeholder="Email"> <br>
+      <label for="password">Password</label>
+      <input type="password" name="password" placeholder="Password">
+      <button class="bg-green-700 text-gray-100" type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  name: 'userLogin',
+  methods: {
+    ...mapActions(['login'])
+  }
+}
+</script>
+
+<style>
+
+</style>
