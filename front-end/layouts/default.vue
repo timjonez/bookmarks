53aa8818bc9b328e99af7513809b6879c0d9de03
Tim Jones <timjonez@protonmail.com>
2021-12-31 20:54:33 -0600

    [front-end/layout] add default layout

---
+++ front-end/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nav-bar />
+    <div class="msg-modal-container msg-modal" :class="{'hidden': !$store.state.messageModalOpen, 'flex': $store.state.messageModalOpen }">
+      <message-modal v-for="message in $store.state.messages" :key="message.id" :message="message" />
+    </div>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue'
+import MessageModal from '../components/MessageModal.vue'
+
+export default {
+  name: 'DefaultLayout',
+  components: {
+    NavBar,
+    MessageModal
+  }
+}
+</script>
+
+<style>
+
+</style>
