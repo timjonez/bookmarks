836b8ddd948048cace75e60b95663a722a45fc26
Tim Jones <timjonez@protonmail.com>
2021-12-31 20:55:16 -0600

    [front-end/componenst/NavBar] add navbar with basic links

---
+++ front-end/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="px-4 py-3 bg-gray-200">
+    <nav class="flex justify-between">
+      <nuxt-link to="/" class="">
+        <h1>Bookmarker</h1>
+      </nuxt-link>
+      <div class="">
+        <nuxt-link to="/login">Login</nuxt-link>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
