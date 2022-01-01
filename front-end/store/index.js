6c57ed0a62a49f470c1159a28b4ca8c4759b78cd
Tim Jones <timjonez@protonmail.com>
2021-12-31 20:58:11 -0600

    [front-end/store/index] add actions and muts for messages

--- front-end/store/index.js
+++ front-end/store/index.js
@@ -2,18 +2,13 @@ import axios from 'axios'
 import { BASEURL } from '../constants'
 
 export const state = () => ({
+  messageModalOpen: false,
   auth: {
     userLoggedIn: false,
     showAuthModal: false,
     token: null
   },
-  errors: [
-    {
-      title: 'Invalid credentials',
-      details: 'The email/password that you provided are invalid. Please try again',
-      seen: false
-    }
-  ],
+  messages: [],
   bookmarks: []
 })
 
@@ -28,9 +23,29 @@ export const actions = {
         }
       )
       context.commit('setToken', userCred.data.token)
+      context.commit('addMessage', {
+        id: null,
+        title: 'Successful Login',
+        details: 'You have successfully logged in!',
+        type: 'success'
+      })
     } catch (error) {
-      console.log(error.response.data.detail)
+      context.commit('addMessage', {
+        id: null,
+        title: 'Error',
+        details: error.response.data.detail,
+        type: 'error'
+      })
     }
+  },
+  removeMessage ({ commit, state }, payload) {
+    commit('removeMessage', payload)
+  },
+  addMessage ({ commit }, message) {
+    commit('addMessage', message)
+  },
+  addBookmark ({ commit }, payload) {
+    commit('addBookmark', payload)
   },
   editBookmark (context, payload) {
     context.commit('editBookmark', payload)
@@ -47,6 +62,22 @@ export const mutations = {
   editBookmark (state, bookmark) {
     const bookmarkIndex = state.bookmarks.findIndex(item => item.id === bookmark.id)
     state.bookmarks[bookmarkIndex].editing = !state.bookmarks[bookmarkIndex].editing
+  },
+  addMessage (state, message) {
+    message.id = state.messages.length + 1
+    state.messages.push(message)
+    state.messageModalOpen = true
+  },
+  removeMessage (state, messageId) {
+    state.messages = state.messages.filter((msg) => {
+      return msg.id !== messageId
+    })
+    if (state.messages.length === 0) {
+      state.messageModalOpen = false
+    }
+  },
+  addBookmark (state, bookmark) {
+    state.bookmarks.push(bookmark)
   },
   saveBookmark (state, bookmark) {
     console.log(bookmark)
