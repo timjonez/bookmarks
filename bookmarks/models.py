51c348633ed8ebfe6e38691f2d691be82f37e565
Tim Jones <timjonez@protonmail.com>
2022-01-03 21:16:21 -0600

    [bookmarks/models] adjust favicon field and add url validators

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,7 +1,8 @@
 import ormar
 from bookmarker.db import BaseMeta
 from users.models import User
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, HttpUrl, validator
+import validators
 from typing import Optional
 
 
@@ -12,8 +13,22 @@ class Bookmark(ormar.Model):
     id: int = ormar.Integer(primary_key=True, autoincrement=True)
     title: str = ormar.String(max_length=128)
     url: HttpUrl = ormar.String(max_length=254)
-    favicon_url: HttpUrl = ormar.String(max_length=254)
+    favicon_url: HttpUrl = ormar.String(max_length=254, nullable=True)
     user: User = ormar.ForeignKey(User, nullable=False)
+
+    @validator('url')
+    def url_is_valid(cls, value):
+        if validators.url(value):
+            return value
+        raise ValueError("Url must be a valid url")
+
+    @validator('favicon_url')
+    def favicon_is_valid(cls, value):
+        if value is None:
+            return value
+        elif validators.url(value):
+            return value
+        raise ValueError("Favicon url must be a valid url")
 
 
 class BookmarkPatchModel(BaseModel):
