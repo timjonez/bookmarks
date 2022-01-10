0a004d58b77d606ac231fe05b49b04d7c998df10
Tim Jones <timjonez@protonmail.com>
2022-01-10 14:30:25 -0600

    [front-end/pages/login] add styling with scoped css

--- front-end/pages/login.vue
+++ front-end/pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1>Login</h1>
-    <form method="post" @submit.prevent="login">
+    <form method="post" @submit.prevent="login" class="card">
+      <h1 class="text-4xl">Log In</h1>
       <label for="email">Email</label>
       <input type="email" name="email" placeholder="Email"> <br>
       <label for="password">Password</label>
@@ -22,6 +22,36 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+div {
+  height: 100%;
+  display: grid;
+}
 
+form {
+  align-self: center;
+  justify-self: center;
+}
+
+h1, label {
+  color: #003049ff;
+}
+
+input {
+  padding: 0.7rem 1.3rem;
+  min-width: 30vw;
+  border-radius: 0.35rem;
+  border: rgb(204, 205, 207)solid 1px;
+}
+
+label {
+  padding-top: 0.7rem;
+}
+
+button {
+  margin-top: 2rem;
+  padding: 0.7rem 1.3rem;
+  background-color: #d62828ff;
+  width: 50%;
+}
 </style>
