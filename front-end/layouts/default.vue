158adb48dff4e194b5ffcf658e1dd879e4294e74
Tim Jones <timjonez@protonmail.com>
2022-01-10 14:28:12 -0600

    [front-end/layouts/default] make elements fullscreen and create class for cards

--- front-end/layouts/default.vue
+++ front-end/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="h-full w-full">
     <nav-bar />
     <div class="msg-modal-container msg-modal" :class="{'hidden': !$store.state.messageModalOpen, 'flex': $store.state.messageModalOpen }">
       <message-modal v-for="message in $store.state.messages" :key="message.id" :message="message" />
@@ -22,8 +22,16 @@ export default {
 </script>
 
 <style>
-input {
-  border: solid 1px;
+html, body, #__nuxt, #__layout {
+  height: 100%;
+  width: 100%;
+}
+
+.card {
+  background-color: rgb(229, 231, 235);
+  padding: 4rem 3rem;
+  display: flex;
+  flex-direction: column;
 }
 
 </style>
