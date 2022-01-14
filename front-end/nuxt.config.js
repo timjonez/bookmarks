ddbbbaf96d395a49a180c5ffcf50895b8117c01c
Tim Jones <timjonez@protonmail.com>
2022-01-14 16:21:25 -0600

    [front-end/nuxt-config] add vue-awesome plugin and also to build

--- front-end/nuxt.config.js
+++ front-end/nuxt.config.js
@@ -22,6 +22,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~plugins/vue-awesome.js',
     '~/plugins/directives.js'
   ],
 
@@ -50,5 +51,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    transpile: [/^vue-awesome/]
   }
 }
