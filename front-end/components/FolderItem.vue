ce114f67e98fe5bf3164aa6913a35a04a3b840ab
Tim Jones <timjonez@protonmail.com>
2022-02-15 20:47:25 -0600

    [front-end/comps/FolderItem] add func to get folder items

--- front-end/components/FolderItem.vue
+++ front-end/components/FolderItem.vue
@@ -3,11 +3,15 @@
     <div class="bookmark-list-item" target="_blank">
       <v-icon name="folder-open" class="ml-2" />
       <p class="ml-3">{{ folder.name }}</p>
+      <button @click.prevent="getFolderItems">Open</button>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import { mapActions } from 'vuex'
+import { BASEURL } from '../constants'
 export default {
   name: 'FolderItem',
   props: {
@@ -16,6 +20,33 @@ export default {
   computed: {
     folder () {
       return this.$store.getters.getFolderById(this.folderId)
+    }
+  },
+  methods: {
+    ...mapActions(['appendFolder']),
+    async getFolderItems () {
+      try {
+        const { data } = await axios.get(
+          `${BASEURL}/folder/${this.folderId}`,
+          {
+            headers: { Authorization: this.$store.state.auth.token }
+          }
+        )
+        data.forEach((folder) => {
+          this.appendFolder(folder)
+        })
+      } catch (error) {
+        let msg = error.response.data.detail
+        if (error.response.status === 401) {
+          msg = 'Please login to view this page'
+        }
+        this.addMessage({
+          id: null,
+          title: 'Error',
+          details: msg,
+          type: 'error'
+        })
+      }
     }
   }
 }
